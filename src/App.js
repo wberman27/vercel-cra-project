c03d91a0c7062f9ba92f2b2925e7780ff402d3a5
Will B <wberman27@gmail.com>
2021-03-25 13:08:16 -0400

    removed state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+ 
   return (
     <main>
       <h1>William's Guided Vercel Project</h1>
